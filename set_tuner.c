8a5971e7d8d59db4ef678d397b5d79ee2c33da3d
Luotao Fu <devtty0@gmail.com>
2013-02-09 16:59:45 +0100

    Fixed station search and load callbacks
    * clear internal playlist upon starting, otherwise the playlist grows every
      time we start.
    * decend search position hen no station can be found at requested pos.
    * fixed debug print.

--- set_tuner.c
+++ set_tuner.c
@@ -36,11 +36,14 @@ int load_stations_playlist(struct mpd_handle *m_handle)
 	int ret = 0;
 	const char* path = DEFAULT_PLS_NAME;
 
-	ret = mpd_playlist_queue_load(m_handle->mpd_obj, path);
 
-	if (!ret)
-		ret = mpd_playlist_queue_commit(m_handle->mpd_obj);
+	ret = mpd_playlist_clear(m_handle->mpd_obj);
+	if (ret)
+		goto out;
 
+	ret = mpd_playlist_load(m_handle->mpd_obj, path);
+
+out:
 	return ret;
 }
 
@@ -53,22 +56,27 @@ int set_tuner(struct mpd_handle *m_handle)
 
 	mpd_Song *mpd_song = NULL;
 
-	mpd_song = mpd_playlist_get_song_from_pos
-		(m_handle->mpd_obj, station_pos);
+	/* if we don't find a station at the position, we go a position down try
+	 * a decended position till position == 0 */
+	while (mpd_song == NULL && station_pos >= 0) {
+		mpd_song = mpd_playlist_get_song_from_pos
+			(m_handle->mpd_obj, station_pos);
+		station_pos--;
+	}
 
 #ifdef DEBUG
 	printf("raw_val: %d, raw_max: %d, max tuner pos: %d,"
 			" tuner position: %d, id: %d\n",
 			m_handle->tuner_raw_val, m_handle->raw_max,
-			m_handle->max_tuner_pos, station_pos,
-			m_handle->mpd_song->id);
+			m_handle->max_tuner_pos, station_pos + 1,
+			mpd_song->id);
 #endif
 
 	if (mpd_song != NULL)
 		ret = mpd_player_play_id(m_handle->mpd_obj,
 				mpd_song->id);
 	else {
-		printf("No stations found at %d\n", station_pos);
+		printf("No stations found\n");
 		ret = 1;
 	}
 
