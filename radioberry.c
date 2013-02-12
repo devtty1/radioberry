72aef7fd7ce3785d4462637b60088f8096e88ec7
Luotao Fu <devtty0@gmail.com>
2013-02-12 20:56:16 +0100

    fixed wrong error handling while failed to retrieve station

--- radioberry.c
+++ radioberry.c
@@ -155,7 +155,7 @@ int main(int argc, const char *argv[])
 	/* disable tuner control if we cannot get any station at all. If one
 	 * station fails, let's process and wait for tuner changes */
 	ret = load_stations_playlist(&m_handle);
-	if (ret > 0) {
+	if (ret == 1) {
 		printf("could not load radio stations list,"
 				" tuner control disabled\n");
 		tuner_control_enabled = 0;
